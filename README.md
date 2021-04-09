15c22a2528d619ed3519010c3982f92a4be12e89
supergizzy <george.isiguzo@yahoo.com>
2021-04-09 14:46:14 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Budget-App-Task
+This Budget App Task is the 4th task of the Zuri Internship.
+
+### Specification
+This Budget App uses a Budget class that can instantiate objects based on different budget categories like food, clothing, and entertainment. These objects should allow for
+1.  Depositing funds to each of the categories
+2.  Withdrawing funds from each category
+3.  Computing category balances
+4.  Transferring balance amounts between categories
---
+++ main.py
@@ -0,0 +1,95 @@
+# create the class
+class Budget:
+    
+    # initialize its attributes
+    def __init__(self, name):
+        self.name = name
+        self.balance = 0
+    
+    # This method handles deposit within a category
+    def deposit(self, amount):
+        
+        # add the credit to the category balance
+        self.balance = amount
+        feedback = "********Deposit Result********\n"
+        feedback += f"Your new balance is ${self.balance} in {self.name} budget\n"
+        feedback += "_________________________________"
+        return feedback
+        
+    # this method handles withdrawal within a category    
+    def withdraw(self, amount):
+        
+        # check if the balance is low
+        if self.balance < amount:
+            feedback = "********Withdrawal Result********\n"
+            feedback += "Insufficient Funds\n"
+            feedback += "_________________________________"
+            return feedback
+        else:
+            # subtract the debit from the category balance
+            self.balance = self.balance - amount
+            
+            feedback = "********Withdrawal Result********\n"
+            feedback += "Transaction was successful\n"
+            # feedback += f"Your new balance is ${self.balance} in {self.name} budget"
+            feedback += "_________________________________"
+            
+            return feedback
+        
+    # this method returns the balance of a category
+    def get_balance(self):
+        # give a reasonable feedback to user
+        feedback = "********Balance Information********\n"
+        feedback += f"The balance for {self.name} is ${self.balance}\n"
+        feedback += "_________________________________"
+        
+        return feedback
+    
+    # this method handles transfer across other categories only
+    def transfer(self, amount, category_object):
+        
+        # check if the user enters a category object into itself
+        if self.name == category_object.name:
+            feedback = "ERROR!\n"
+            feedback += "You can not transfer within the same category\n"
+            feedback += "You can only deposit within a category"
+            return feedback
+        
+        # check if the balance is low
+        if self.balance < amount:
+            return "Insufficient Funds"
+        else:
+            # transfer the money
+            self.balance -= amount
+            category_object.balance += amount
+            
+            feedback = "********Transaction Result********\n"
+            feedback += "Transfer was successful\n"
+            feedback += f"The balance for {self.name} is ${self.balance}\n"
+            feedback += f"The balance for {category_object.name} is ${category_object.balance}\n"
+            feedback += "_________________________________"
+            
+            return feedback 
+            
+
+
+
+# instantiating 2 objects from the budget class
+food_budget = Budget("food")
+clothing_budget = Budget("clothing")
+
+# deposit money into 
+print(food_budget.deposit(2000))
+print(clothing_budget.deposit(5000))
+
+print(food_budget.withdraw(2500)) # this should return insufficient funds
+print(clothing_budget.withdraw(1000)) # this should return transaction successful
+
+# get balances for both categories
+print(food_budget.get_balance()) 
+print(clothing_budget.get_balance())
+
+# transfer between categories
+print(clothing_budget.transfer(1500, food_budget))
+print(food_budget.transfer(200, clothing_budget))
+
